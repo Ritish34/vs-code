99a09da87bbe9a99ff1a170348cb42d8230cea51
Ritish <ritish.inexture@gmail.com>
2022-07-06 11:18:10 +0530

    First commit

---
+++ Convertion.java
@@ -0,0 +1,6 @@
+//Map to List
+ ArrayList<Integer> keyList = new ArrayList<Integer>(myMap.keySet());
+ ArrayList<String> valueList = new ArrayList<String>(myMap.values());
+
+ //Convert List to Array(Object)
+ TypeA[] array = a.toArray(new TypeA[a.size()]);
